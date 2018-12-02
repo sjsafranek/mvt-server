c38ed32cae943752d78752a452c72ba55db3a661
stefan safranek <stefan@stefan.stefan>
2018-12-02 12:08:48 -0800

    cleanup

--- db_setup.sh
+++
@@ -1,6 +0,0 @@
-sudo -u postgres psql -c "CREATE USER geodev WITH PASSWORD 'dev'"
-sudo -u postgres psql -c "CREATE DATABASE geodev"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE geodev to geodev"
-sudo -u postgres psql -c "ALTER USER geodev WITH SUPERUSER"
-
-PGPASSWORD=dev psql -d geodev -U geodev -f database.sql
