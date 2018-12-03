fbfa35e494ff896dadbc0d1866faa6e08701e464
Stefan Safranek <stefan@stefan>
2018-12-03 14:53:08 -0800

    logging response status code

--- middleware.go
+++ middleware.go
@@ -5,6 +5,19 @@ import (
 	"time"
 )
 
+// https://ndersson.me/post/capturing_status_code_in_net_http/
+// https://upgear.io/blog/golang-tip-wrapping-http-response-writer-for-middleware/
+type LoggingResponseWriter struct {
+	http.ResponseWriter
+	status int
+}
+
+func (self *LoggingResponseWriter) WriteHeader(code int) {
+	self.status = code
+	self.ResponseWriter.WriteHeader(code)
+}
+
+
 func LoggingMiddleWare(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
@@ -12,12 +25,14 @@ func LoggingMiddleWare(next http.Handler) http.Handler {
 		// before
 		logger.Debugf(" In %v %v %v", r.RemoteAddr, r.Method, r.URL)
 
+		// Initialize the status to 200 in case WriteHeader is not called
+		lrw := LoggingResponseWriter{w, 200}
+
 		// during
-		next.ServeHTTP(w, r)
+		next.ServeHTTP(&lrw, r)
 
 		// end
-		// logger.Debugf("Out %v %v %v %v", r.RemoteAddr, r.Method, r.URL, w.Header())
-		logger.Debugf("Out %v %v %v %v", r.RemoteAddr, r.Method, r.URL, time.Since(start))
+		logger.Debugf("Out %v %v %v [%v] %v", r.RemoteAddr, r.Method, r.URL, lrw.status, time.Since(start))
 	})
 }
 
